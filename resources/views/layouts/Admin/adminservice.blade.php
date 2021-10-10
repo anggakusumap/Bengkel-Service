e31461b89fc19de8de06ab41b5f171658555fbc6
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-10-11 02:16:32 +0800

    gas

--- resources/views/layouts/Admin/adminservice.blade.php
+++ resources/views/layouts/Admin/adminservice.blade.php
@@ -79,7 +79,7 @@
                 
                 @endif
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="{{ route('dashboardsso') }}">
+                    <a class="dropdown-item" href="https://sso.bengkel-kuy.com/sso">
                         <div class="dropdown-item-icon"><i data-feather="columns"></i></div>
                         Dashboard SSO
                     </a>
