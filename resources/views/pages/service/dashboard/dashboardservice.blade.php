a3650e913e5c538e9b3ff163648690d4e6c07e6f
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-10-11 02:35:30 +0800

    Update dashboardservice.blade.php

--- resources/views/pages/service/dashboard/dashboardservice.blade.php
+++ resources/views/pages/service/dashboard/dashboardservice.blade.php
@@ -40,7 +40,7 @@
                             Anda untuk melakukan penerimaan dan pengerjaan service.
                         </p>
 
-                        <a class="btn btn-primary btn-sm px-3 py-2" href="{{ route('pelayananservice.index') }}">
+                        <a class="btn btn-primary btn-sm px-3 py-2" href="{{ route('penerimaanservice.create') }}">
                             Get Started
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
